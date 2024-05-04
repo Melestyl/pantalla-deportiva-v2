1ef377c10e4e7e745a025b484f0df7860b6c8445
Melestyl <tom.telcal@icloud.com>
2024-05-04 04:48:39 +0200

    The host player can now invite someone (server-side). When the other one says yes, a court is reserved and both of them connect to the court.

--- server/player_functions.h
+++ server/player_functions.h
@@ -48,4 +48,30 @@ typedef struct player_node player_node_t;
  */
 void invited_player(socket_t* client_socket, char* data);
 
+/**
+ * @fn player_t* invite_player(int id)
+ * @brief Function to invite a player
+ * @param host_socket: socket of the host
+ * @param id: id of the player to invite
+ * @param host: host player structure
+ * @param partner_player: player structure filled if the player has accepted the invitation
+ * @return int: 1 if the player has accepted the invitation (or exists), 0 otherwise
+ */
+int invite_player(socket_t host_socket, int id, player_t* host, player_t* partner_player);
+
+/**
+ * @fn void list_players(socket_t host_socket)
+ * @brief Send to host_socket the list of available players
+ * @param host_socket: socket of the host
+ */
+void list_players(socket_t* host_socket);
+
+/**
+ * @fn void host_player(socket_t client_socket, buffer_t ip, int port)
+ * @brief Function to handle a player who invites
+ * @param client_socket: socket of the current player
+ * @param data: data received from the client when authenticating
+ */
+void host_player(socket_t* client_socket, char* data);
+
 #endif //PANTALLA_DEPORTIVA_V2_PLAYER_FUNCTIONS_H
